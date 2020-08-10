b342e64484cd8bc45508a11792a3b3dcb079a6c8
Gokul <gokul.subburaj@adcltech.com>
2020-08-10 11:01:19 +0530

    gokul work dashboard new

--- src/app/Components/machine-view/machine-view.component.ts
+++ src/app/Components/machine-view/machine-view.component.ts
@@ -24,6 +24,9 @@ export class MachineViewComponent implements OnInit {
   semicircle;
   fullcircle;
   testsample;
+  machie_statusnew;
+  machie_statustemp;
+  sp_speed;
   constructor(private nav:NavbarService,private route:ActivatedRoute,private service:MachineViewService) {
     this.nav.show();
     setInterval(() => {this.today = Date.now()}, 1);
@@ -31,35 +34,21 @@ export class MachineViewComponent implements OnInit {
     this.myLoader = true;
     this.service.dashboard_full(this.machine).subscribe(res =>{
       this.machie_status =res;
+      this.machie_statusnew =this.machie_status.puls_code;
+      this.machie_statustemp =this.machie_status.axis_tem;
       
       this.testgokul = this.machie_status.axis_load;
       this.testsample = this.machie_status.puls_code;
-      console.log(this.machie_status.puls_code)
       this.myLoader = false;
       this.spindle();
       this.repeatchart = [];
       this.repeatpulse = [];
      
-      // if(this.machie_status.spindle_load <= 0){
-      //   $scope.pop = "false";
-      //   console.log($scope.pop);
-      // }else{
-      //   $scope.pop = "true";
-      //   console.log($scope.pop);
-      // }
    for (let i = 0; i < Object.values(this.machie_status.axis_load).length; i++) {
     this.repeatchart.push(this.vall(this.axis(Object.values(this.machie_status.axis_load)[i])))
     }
-
-    // for (let i = 0; i < Object.values(this.machie_status.puls_code).length; i++) {
-    //   this.repeatpulse.push(this.val(this.pul(Object.values(this.machie_status.puls_code)[i])))
-    //   }
-
-  
      this.feedrate = this.machie_status.feed_rate;
-     
      this.spindlespeed = this.machie_status.spindle_speed; 
-
     });
    }
    axis(ax){
@@ -93,36 +82,22 @@ export class MachineViewComponent implements OnInit {
 
 
   spindle(){
-
-    /// Feed Rate Chart
-    var container = Highcharts.chart('container2', {
-      credits: {
-        enabled: false
-      },
+    var containergokul = Highcharts.chart('containergokul',{
       chart: {
         type: 'gauge',
         plotBackgroundColor: null,
         plotBackgroundImage: null,
         plotBorderWidth: 0,
-        plotShadow: false,
-        // height: 200,
-        // width: 200,
-        fill: 'transparent',
-        backgroundColor: {
-          backgroundColor: '#262932',
-        }
+        plotShadow: false
       },
       title: {
-        text: 'Feed',
-        y: 30,
         style: {
-          fontSize: '14px',
-          color: '#fff',
-          lineheight: 10,
+          color: '#FFFFFF',
           font: 'bold 14px "Trebuchet MS", Verdana, sans-serif'
         },
+        text: 'Spindle'+ '\xa0'+'Speed',
+        y:30
       },
-
       pane: {
         startAngle: -90,
         endAngle: 90,
@@ -133,113 +108,6 @@ export class MachineViewComponent implements OnInit {
         labels: {
           enabled: true,
           x: 35, y: -10
-        },
-
-        tickPositions: [],
-        minorTickLength: 0,
-        min: 0,
-        max: 10000,
-        plotBands: [{
-          from: 0,
-          to: 5000,
-          color: {
-            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-            stops: [
-              [0, '#40aa3e'], //green
-              [1, '#59db57'] //red
-            ]
-          },
-          thickness: '40%'
-        }, {
-          from: 5000,
-          to: 10000,
-          color: {
-            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-            stops: [
-              [0, '#fd6363'], //green
-              [1, '#c41a1a'] //red
-            ]
-          },
-          thickness: '40%'
-        }]
-      },
-      exporting: {
-        enabled: false
-      },
-      series: [{
-        name: 'Spindle Speed',
-        // data: ['80'spindlespeed],
-        background: '#fff',
-        data: [{
-          color: '#fff',
-          background: '#fff',
-          fillL: '#fff',
-          radius: '100%',
-          innerRadius: '80%',
-          y: 0
-        }],
-        color: {
-          linearGradient: [1, 1, 1, 1],
-          stops: [
-            [0.4, '#fff'],
-            [0.1, '#fff']
-          ]
-        },
-        dataLabels: {
-          format: '<span style="font-size:14px;color:grey;">{y} m/min</span></div>',
-          y: 10,
-          borderWidth: 0,
-          fill: '#ccc',
-          color: '#ccc'
-        },
-        tooltip: {
-          valueSuffix: 'm/min'
-        },
-      
-      }]
-
-    });
-    var container = Highcharts.chart('container', {
-      credits: {
-        enabled: false
-      },
-      chart: {
-        type: 'gauge',
-        plotBackgroundColor: null,
-        plotBackgroundImage: null,
-        plotBorderWidth: 0,
-        plotShadow: false,
-        // height: 200,
-        // width: 180,
-        fill: 'transparent',
-        backgroundColor: {
-          backgroundColor: '#262932',
-        }
-      },
-      title: {
-        style: {
-          fontSize: '14px',
-          color: '#fff',
-          font: 'bold 14px "Trebuchet MS", Verdana, sans-serif'
-        },
-        text: 'Spindle' + '\xa0' + 'Speed',
-        y: 30
-      },
-
-      pane: {
-        startAngle: -90,
-        endAngle: 90,
-        background: null
-      },
-
-      yAxis: {
-        labels: {
-          enabled: true,
-          x: 35, y: -10
-        },
-
-        dial: {
-          backgroundColor: 'blue'
         },
         tickPositions: [],
         minorTickLength: 0,
@@ -274,35 +142,106 @@ export class MachineViewComponent implements OnInit {
       },
       series: [{
         name: 'Spindle Speed',
-        // data: ['80'spindlespeed],
-        data: [{
-          color: '#fff',
-          fillL: '#fff',
-          radius: '100%',
-          innerRadius: '80%',
-          y: 0
-        }],
+        data: [this.machie_status.spindle_speed],
         color: {
           linearGradient: [0, 0, 300, 0],
           stops: [
-            [0.4, '#fff'],
-            [0.1, '#fff']
+            [0.4, '#FF0000'],
+            [0.1, '#55BF3B']
           ]
         },
         dataLabels: {
           format: '<span style="font-size:14px;color:grey;">{y} RPM</span></div>',
           y: 10,
-          borderWidth: 0,
-          fill: '#ccc',
-          color: '#ccc'
+          borderWidth: 0
         },
         tooltip: {
           valueSuffix: ' RPM'
-        },
-       
-      }],
-
+        }
+      }]
     });
+
+    var FeedRategokul = Highcharts.chart('FeedRategokul',{
+      
+      chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+
+      },
+      title: {
+        style: {
+          color: '#FFFFFF',
+          font: 'bold 14px "Trebuchet MS", Verdana, sans-serif'
+        },
+        text: 'Feed',
+        y:30
+      },
+      pane: {
+        startAngle: -90,
+        endAngle: 90,
+        background: null
+      },
+
+      yAxis: {
+        labels: {
+          enabled: true,
+          x: 35, y: -50
+        },
+        tickPositions: [],
+        minorTickLength: 0,
+        min: 0,
+        max: 5000,
+        plotBands: [{
+          from: 0,
+          to: 2500,
+          color: {
+            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+              [0, '#40aa3e'], //green
+              [1, '#59db57'] //red
+            ]
+          },
+          thickness: '40%'
+        }, {
+          from: 2500,
+          to: 5000,
+          color: {
+            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+              [0, '#fd6363'], //green
+              [1, '#c41a1a'] //red
+            ]
+          },
+          thickness: '40%'
+        }]
+      },
+
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        name: 'Feed Rate',
+        data: [this.machie_status.feed_rate], 
+        color: {
+          linearGradient: [0, 0, 300, 0],
+          stops: [
+            [0.4, '#FF0000'],
+            [0.1, '#55BF3B']
+          ]
+        },
+        dataLabels: {
+          format: '<span style="font-size:15px;color:grey;">{y} m/min </span></div>',
+          y: 10,
+          borderWidth: 0
+        },
+        tooltip: {
+          valueSuffix: ' m/min'
+        }
+      }]
+    })
   }
 
 }
